05de7294e1a9713926c1fc3fb1c14ad42abb7c18
Manuel Esparza <manoloesparta@gmail.com>
2023-06-08 00:34:22 -0600

    app

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return os.environ.get("POD_ID")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="80")
